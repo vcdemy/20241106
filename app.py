4465801702339ce8920e5c27e8e672a535c800d7
vcdemy <100576663+vcdemy@users.noreply.github.com>
2024-11-13 17:00:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import folium
 
 app = Flask(__name__)
 
@@ -6,9 +7,13 @@ app = Flask(__name__)
 def index():
     return "Hello World!"
 
-@app.route("/<name>")
-def greet(name):
-    return "Hello " + name.upper()
+@app.route("/map")
+def map():
+    m = folium.Map(location=[45.5236, -122.6750], zoom_start=13)
+
+    # Convert the map to an HTML string
+    map_html = m.get_root().render()
+    return map_html
 
 if __name__=="__main__":
     app.run(debug=True)
